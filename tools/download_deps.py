75f515a7ddce2dafde8b8e1babaf56164af0f028
skylerlee <skyler.ac.lee@gmail.com>
2020-01-29 01:08:22 +0800

    feat: update script

--- tools/download_deps.py
+++ tools/download_deps.py
@@ -1,7 +1,9 @@
 import os
 import json
 import shutil
+import hashlib
 import tempfile
+import tarfile
 from urllib import request
 
 
@@ -9,8 +11,8 @@ class DepInfo:
     def __init__(self, d):
         self.name = d['name']
         self.type = d['type']
-        self.version = d['version']
         self.url = d['url']
+        self.checksum = d.get('checksum')
 
 
 def find_project_root(lookup_file='.git'):
@@ -22,21 +24,47 @@ def find_project_root(lookup_file='.git'):
     return p
 
 
-def download_from_url(url, target_file):
+def clean_target_dir(target):
+    if os.path.exists(target):
+        shutil.rmtree(target)
+
+
+def download_from_url(url, target):
     with request.urlopen(url) as resp:
-        with open(target_file, 'wb') as fp:
+        with open(target, 'wb') as fp:
             shutil.copyfileobj(resp, fp)
 
 
+def extract_zip_file(file, target):
+    with tarfile.open(file) as arch:
+        arch.extractall(target)
+
+
+def verify_checksum(file, checksum):
+    md5 = hashlib.md5()
+    with open(file, 'rb') as fp:
+        while True:
+            buf = fp.read(4096)
+            if not buf:
+                break
+            md5.update(buf)
+    digest = md5.hexdigest()
+    if digest != checksum:
+        raise Exception('Mismatched checksum')
+
+
 def download_and_extract_zip(dep, target_dir):
-    url = dep.url.format(version=dep.version)
     tmp = tempfile.mktemp()
-    download_from_url(url, tmp)
+    download_from_url(dep.url, tmp)
+    if dep.checksum:
+        verify_checksum(tmp, dep.checksum)
+    extract_zip_file(tmp, target_dir)
 
 
 def download_dependencies(dep_list, dest_dir):
     for dep in dep_list:
         target_dir = os.path.join(dest_dir, dep.name)
+        clean_target_dir(target_dir)
         if dep.type == 'zip':
             download_and_extract_zip(dep, target_dir)
         else:
