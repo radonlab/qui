b431a96ca8f4fd721870f5fd9447a56f0ed620c6
skylerlee <skyler.ac.lee@gmail.com>
2020-03-22 04:38:44 +0800

    feat: apply code

--- tools/build_skia.py
+++ tools/build_skia.py
@@ -1,9 +1,12 @@
 import os
 import subprocess
+from buildutils import find_project_root
 
 
 def build():
-    os.chdir('../third_party/skia')
+    root_dir = find_project_root()
+    skia_dir = os.path.join(root_dir, 'third_party', 'skia')
+    os.chdir(skia_dir)
     subprocess.call(['bin/gn', 'gen', 'out/Shared', '--args=is_official_build=true is_component_build=true'])
     subprocess.call(['ninja', '-C', 'out/Shared'])
 
--- tools/download_deps.py
+++ tools/download_deps.py
@@ -2,6 +2,7 @@ import os
 import json
 import shutil
 import subprocess
+from buildutils import find_project_root
 
 
 class DepInfo:
@@ -17,15 +18,6 @@ class DepInfo:
 
     def checkout(self):
         subprocess.call(['git', 'checkout', self.branch])
-
-
-def find_project_root(lookup_file='.git'):
-    p = os.path.dirname(os.path.abspath(__file__))
-    while not os.path.exists(os.path.join(p, lookup_file)):
-        if p == os.path.dirname(p):
-            raise Exception('Failed to find "{}"'.format(lookup_file))
-        p = os.path.dirname(p)
-    return p
 
 
 def clean_target_dir(target):
