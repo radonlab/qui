29d41307f99e3c4fb41ff0ff9a3f035e055bb862
skylerlee <skyler.ac.lee@gmail.com>
2020-03-22 05:51:40 +0800

    feat: update

--- tools/build_skia.py
+++ tools/build_skia.py
@@ -3,11 +3,30 @@ import subprocess
 from buildutils import find_project_root
 
 
+build_args = {
+    'is_official_build': True,
+    'is_component_build': True,
+}
+
+
+def stringify_args(args):
+    pairs = []
+    for key, val in args.items():
+        sval = ''
+        if type(val) is bool:
+            sval = 'true' if val else 'false'
+        else:
+            sval = str(val)
+        pairs.append('{}={}'.format(key, sval))
+    return ' '.join(pairs)
+
+
 def build():
     root_dir = find_project_root()
     skia_dir = os.path.join(root_dir, 'third_party', 'skia')
     os.chdir(skia_dir)
-    subprocess.call(['bin/gn', 'gen', 'out/Shared', '--args=is_official_build=true is_component_build=true'])
+    args = stringify_args(build_args)
+    subprocess.call(['bin/gn', 'gen', 'out/Shared', '--args={}'.format(args)])
     subprocess.call(['ninja', '-C', 'out/Shared'])
 
 
