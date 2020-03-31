08457df1010f300a53dd6ef1c565b8f1e844ce8c
skylerlee <skyler.ac.lee@gmail.com>
2020-04-01 02:04:35 +0800

    chore: add include paths

--- cmake/FindSkia.cmake
+++ cmake/FindSkia.cmake
@@ -19,7 +19,23 @@ find_library(SKIA_LIBRARY
 
 if(SKIA_INCLUDE_DIR AND SKIA_LIBRARY)
   set(SKIA_FOUND TRUE)
-  set(SKIA_INCLUDE_DIRS ${SKIA_INCLUDE_DIR})
+  set(SKIA_INCLUDE_DIRS
+    "${SKIA_INCLUDE_DIR}/android"
+    "${SKIA_INCLUDE_DIR}/c"
+    "${SKIA_INCLUDE_DIR}/codec"
+    "${SKIA_INCLUDE_DIR}/config"
+    "${SKIA_INCLUDE_DIR}/core"
+    "${SKIA_INCLUDE_DIR}/docs"
+    "${SKIA_INCLUDE_DIR}/effects"
+    "${SKIA_INCLUDE_DIR}/encode"
+    "${SKIA_INCLUDE_DIR}/gpu"
+    "${SKIA_INCLUDE_DIR}/atlastext"
+    "${SKIA_INCLUDE_DIR}/pathops"
+    "${SKIA_INCLUDE_DIR}/ports"
+    "${SKIA_INCLUDE_DIR}/svg"
+    "${SKIA_INCLUDE_DIR}/utils"
+    "${SKIA_INCLUDE_DIR}/utils/mac"
+  )
   set(SKIA_LIBRARIES ${SKIA_LIBRARY})
 else()
   set(SKIA_FOUND FALSE)
