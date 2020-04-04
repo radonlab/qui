09d5c9f2a5ae8c75dc963a54ee0c8cf820010901
skylerlee <skyler.ac.lee@gmail.com>
2020-04-04 17:12:36 +0000

    feat: update example

--- examples/simple.cc
+++ examples/simple.cc
@@ -1,8 +1,10 @@
 #include "GLFW/glfw3.h"
+#include "context.h"
 
 int main(int argc, const char* argv[]) {
   GLFWwindow* window;
   glfwInit();
+  qui_init_context();
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
