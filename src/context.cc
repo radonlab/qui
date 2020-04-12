d1c3c94d7a5beb8e40e45ebab1a0a83e56a3a41a
skylerlee <skyler.ac.lee@gmail.com>
2020-04-12 07:43:51 +0000

    feat: add context redrawing

--- src/context.cc
+++ src/context.cc
@@ -3,8 +3,11 @@
 #include "GLFW/glfw3.h"
 #include "GrContext.h"
 #include "gl/GrGLAssembleInterface.h"
+#include "SkSurface.h"
 
 namespace qui {
+
+static sk_sp<GrContext> context;
 
 static GrGLFuncPtr get_glfw_proc(void* ctx, const char name[]) {
   return glfwGetProcAddress(name);
@@ -18,11 +21,31 @@ bool InitContext() {
   if (gl == nullptr) {
     return false;
   }
-  sk_sp<GrContext> context = GrContext::MakeGL(gl);
+  context = GrContext::MakeGL(gl);
   if (context == nullptr) {
     return false;
   }
   return true;
+}
+
+bool RescaleContext() {
+  GrGLFramebufferInfo buf_info;
+  buf_info.fFBOID = 0;
+  buf_info.fFormat = GL_RGBA8;
+  GrBackendRenderTarget render_target(800, 500, 0, 0, buf_info);
+  sk_sp<SkSurface> surface = SkSurface::MakeFromBackendRenderTarget(
+      context.get(),
+      render_target,
+      kTopLeft_GrSurfaceOrigin,
+      kRGBA_8888_SkColorType,
+      nullptr,
+      nullptr
+  );
+  SkCanvas* canvas = surface->getCanvas();
+  SkPaint paint;
+  paint.setColor(SK_ColorWHITE);
+  canvas->drawPaint(paint);
+  context->flush();
 }
 
 }  // namespace qui
--- src/context.h
+++ src/context.h
@@ -6,6 +6,7 @@
 namespace qui {
 
 bool InitContext();
+bool RescaleContext();
 
 }  // namespace qui
 
